6debd4f36f694211d0c5f426af8ef0c79a9f04ef
DiegoPaez2 <diegolexx99@gmail.com>
2019-09-25 22:36:51 -0500

    ordering my repository

---
+++ Assignment/Hw05SumNumbers.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workshop;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Add {
+
+    public static void main(String[] args) {
+        int add;
+        int number = 3;
+
+        for (int i = 0; i < 11; i++) {
+            add = number + i;
+            System.out.println(number + "+" + i + "=" + add);
+
+        }
+
+    }
+}
+
+
