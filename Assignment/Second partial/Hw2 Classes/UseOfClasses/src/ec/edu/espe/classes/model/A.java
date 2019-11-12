69be62056fa6020a6ee0c496fd522236ea8ecdad
DiegoPaez2 <diegolexx99@gmail.com>
2019-11-11 22:04:39 -0500

    Homework 2

---
+++ Assignment/Second partial/Hw2 Classes/UseOfClasses/src/ec/edu/espe/classes/model/A.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.classes.model;
+
+public class A {
+
+    private int m;
+    private float n;
+    private B b;
+
+    /**
+     *
+     * @param c
+     * @return 
+     */
+    public D m(C c) {
+
+        return D;
+    }
+
+}
---
+++ Assignment/Second partial/Hw2 Classes/UseOfClasses/src/ec/edu/espe/classes/view/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.classes.view;
+
+import ec.edu.espe.classes.model.A;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        A object1 = new A();
+        A object2 = new A();
+        A object3 = new A();
+    }
+}
